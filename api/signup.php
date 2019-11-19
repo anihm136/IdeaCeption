0f4c6fa368e012babdb717142dff1e4a769c7fea
Anirudh <anihm136@gmail.com>
2019-11-19 12:53:28 +0530

    Changed add to have title page

--- api/signup.php
+++ api/signup.php
@@ -1,19 +1,22 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-type: text/plain");
+if ($_REQUEST["method"] == "POST") {
+  header("Access-Control-Allow-Origin: *");
+  header("Content-type: text/plain");
 
-include_once("../backend/config/database.php");
-include_once("../backend/models/user.php");
+  include_once("../backend/config/database.php");
+  include_once("../backend/models/user.php");
 
-$database = new Database();
-$db = $database->connect();
+  $database = new Database();
+  $db = $database->connect();
 
-$user = new User($db);
+  $user = new User($db);
 
-$user->name = $_POST['userName'];
-$user->email = $_POST['userMail'];
-$user->passwd = $_POST['userPass'];
+  $user->name = $_POST['userName'];
+  $user->email = $_POST['userMail'];
+  $user->passwd = $_POST['userPass'];
 
-$user->signUp();
+  $user->signUp();
+} else {
+  header("Location: ../signup");
+}
 ?>
-
